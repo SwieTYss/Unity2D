eb8ba94bc38a794ea9c53834f51d3626260825bc
Piotr Janiuk <piotr.janiuk@outbox.pl>
2016-08-26 15:49:48 +0200

    today work

---
+++ Gra2D/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+    Animator anim;
+    bool dirToRight = true;
+
+	// Use this for initialization
+	void Start () {
+
+        anim = GetComponent<Animator>();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        float horizontalMove = Input.GetAxis("Horizontal");
+
+        anim.SetFloat("speed", Mathf.Abs(horizontalMove));
+
+        dirToRight = dirToRight;
+
+	}
+}
